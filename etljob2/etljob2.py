8eb674f7195c4cbf1b4ca39b114b21ee878307ba
Swamy Naidu Lenka <110302879+SwamyNaiduLenka@users.noreply.github.com>
2024-06-23 10:47:58 +0530

    Synchronizing job etljob2 from AWS Glue at 2024/06/23 05:17:58

---
+++ etljob2/etljob2.py
@@ -0,0 +1,21 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+df =spark.read.format("csv").load("s3://bucketfortesters/employee_data.csv")
+
+df.write.format("csv").save("s3://bucketfortesters/output/june22_1")
+
+job.commit()
